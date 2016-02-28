b152d5024c779440b02d5222a861520c67faa5fe
Chrisu <Chrisu>
2016-02-28 21:36:03 +0100

    plottest.py added

--- hello.py
+++ hello.py
@@ -1,7 +1,27 @@
-a = 18.73 % 4
+import csv
+import matplotlib.pyplot as plt
+import numpy as np
 
-print(a)
+data = np.genfromtxt('../../Temp_Files/test.csv', delimiter=' ', names=['x','y'])
+data2 = np.loadtxt('../../Temp_Files/test.csv',delimiter=' ')
 
-str1 = 'bla123412334123'
+# print (data['x'])
 
-print(len(str1))
+
+# fig, (ax1, ax2) = plt.subplots(1, 2, sharey=True)
+# ax1.plot(data['x'], data['y'], color='r')
+# ax1.grid(True)
+# ax2.plot(data2[:,0], data2[:,2], color='g')
+# ax2.grid(True)
+
+## Two subplots, unpack the output array immediately
+# f, (ax1, ax2) = plt.subplots(1, 2, sharey=True)
+# ax1.plot(data['x'], data['y'], color='r')
+# ax1.set_title('Sharing Y axis')
+# ax2.scatter(data2[:,0], data2[:,1])
+
+# plt.plot(data['x'], data['y'], color='r')
+plt.plot(data['x'], data['y'], 'r', data2[:,0], data2[:,1]/2, 'g')
+plt.grid(True)
+# plt.plot(data2[:,0], data2[:,1], color='g')
+plt.show()
